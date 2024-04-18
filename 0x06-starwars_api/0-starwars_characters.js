90d631641e7e5657da521342a85973fd3dc0a522
crispusomollo <crispusomollo@gmail.com>
2024-04-18 22:21:02 +0300

    Write a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,43 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie.
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as  list in the /films/ endpoint
- */
-
+//the above line species to the interpreter to be used for the script. in this case it is the Node.js
 const request = require('request');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.alx-tools.com/api/films/';
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async (err, res, body) => {
-  if (err) return console.error(err);
+//This line imports the 'request' module, to make HTTP requests in Node.js. It allows the script to make API requests to fetch data.
+const API_URL = 'https://swapi-api.hbtn.io/api';
+//This line defines the base URL for the Star Wars API.
 
-  // find URLs of each character in the film as a list obj
-  const charURLList = JSON.parse(body).characters;
+if (process.argv.length > 2) {
+  //This condition checks whether command-line arguments are present running the script. 
+  //process.argv is an array containing the command-line arguments passed to the Node.js process.
+  //process.argv.length is checked to ensure that there is at least one additional argument (beyond the default arguments passed to Node.js).
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    //Here, an HTTP GET request is made to the Star Wars API to fetch information about a specific film. 
+    //The process.argv[2] represents the first additional argument provided in the command line (index 0 represents 'node', index 1 represents the script file). 
+    //This argument specifies the film ID in the API URL.
+    if (err) {
+      console.log(err);
+    }
+    //If an error occurs during the API request, it will be logged to the console.
+    const charactersURL = JSON.parse(body).characters;
+    //The response body from the API request is parsed as JSON (JSON.parse(body)), and the characters property is extracted. 
+    //This property contains URLs to the characters' information related to the specified film.
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+      //For each character URL fetched from the film data, a new Promise is created. 
+      //This Promise is used to make individual requests to the character URLs and resolve with the character names.
 
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, res, body) => {
-        if (err) return console.error(err);
 
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-}); 
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
+//Promise.all is used to handle all the promises created for character names. 
+//It waits for all the promises to resolve and then collects the character names in an array (names). 
+//Finally, it prints the character names separated by newline characters ('\n') to the console.
