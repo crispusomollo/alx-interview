2c803bffc87f16ed0f3b92b910a7fb686cb93930
crispusomollo <crispusomollo@gmail.com>
2024-05-14 17:32:33 +0300

    The challenge involves determining the winner of a game based on the strategic removal of prime numbers and their multiples from a set of consecutive integers.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,57 +3,33 @@
 
 
 def isWinner(x, nums):
-    def isPrime(num):
-
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def findNextPrime(curr):
-        next_prime = curr + 1
-        while not isPrime(next_prime):
-            next_prime += 1
-        return next_prime
-
-    # Helper function to simulate the game for a given round
-    def simulateGame(num):
-        # If the number is even, Maria wins
-        if num % 2 == 0:
-            return "Maria"
-        # If the number is odd and prime, Ben wins
-        if isPrime(num):
-            return "Ben"
-        # Otherwise, determine the winner based on the next prime number
-        next_prime = findNextPrime(num)
-        # If the next prime is odd, Maria wins
-        if next_prime % 2 != 0:
-            return "Maria"
-        # Otherwise, Ben wins
-        return "Ben"
-
-    # Count the number of wins for each player
-    maria_wins = 0
-    ben_wins = 0
-    for num in nums:
-        winner = simulateGame(num)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
+    max_num = max(nums)
 
-# Example usage:
-x = 3
-nums = [4, 5, 1]
-print(isWinner(x, nums))  # Output: "Ben"
+    filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            filter[j] = False
+    filter[0] = filter[1] = False
+    y = 0
+    for i in range(len(filter)):
+        if filter[i]:
+            y += 1
+        filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
+
+
+if __name__ == "__main__":
+    assert isWinner(5, [2, 5, 1, 4, 3]) == "Ben", "Expected Ben"
 
