9fbc24eecda03f17da6f91f860bfb307e83b32fa
crispusomollo <crispusomollo@gmail.com>
2024-05-14 17:36:55 +0300

    The challenge involves determining the winner of a game based on the strategic removal of prime numbers and their multiples from a set of consecutive integers.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,33 +3,25 @@
 
 
 def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
+    """Determines the winner of a prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
         return None
-    max_num = max(nums)
-
-    filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not filter[i]:
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
             continue
-        for j in range(i * i, max_num + 1, i):
-            filter[j] = False
-    filter[0] = filter[1] = False
-    y = 0
-    for i in range(len(filter)):
-        if filter[i]:
-            y += 1
-        filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
         return None
-    if player1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
-
-
-if __name__ == "__main__":
-    assert isWinner(5, [2, 5, 1, 4, 3]) == "Ben", "Expected Ben"
-
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
